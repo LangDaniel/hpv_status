371b8891959224ddbc8c56bbb38315e39cc343af
LangDaniel <lang@helmholtz-muenchen.de>
2020-11-30 16:31:00 +0100

    fixed path

--- utils/convert.sh
+++ utils/convert.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 dir=$(dirname $1)
-echo $dir
+file=$(basename $1)
+echo dir: $dir
+echo file: $file
 
-sudo docker run -it -v $dir:/jobs -w /jobs caffe python convert.py $1
+sudo docker run\
+    -it\
+    -v $PWD:/jobs\
+    -w /jobs\
+    -v $dir:/data\
+    caffe_weights2hdf5 python convert.py /data/$file
